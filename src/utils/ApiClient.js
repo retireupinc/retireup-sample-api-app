d9f4b2ad68d8036cd455e714ae5ae0a1438c41fb
Vicente Chavez <vchavez92780@gmail.com>
2021-03-19 08:57:48 -0400

    Cleaned up api wrapper

--- src/utils/ApiClient.js
+++ src/utils/ApiClient.js
@@ -5,37 +5,37 @@ const methods = ["get", "post", "put", "patch", "del"];
 const formatUrl = (path) => (path[0] !== "/" ? `/${path}` : path);
 
 let accessToken;
+let accessTokenExpiresAt;
+
 const getAccessToken = async () => {
   if (!accessToken && window.localStorage.getItem("accessToken")) {
-    accessToken = {
-      accessToken: window.localStorage.getItem("accessToken"),
-      expiresAt: Number(window.localStorage.getItem("accessTokenExpiresAt")),
-    };
+    accessToken = window.localStorage.getItem("accessToken");
+    accessTokenExpiresAt = Number(
+      window.localStorage.getItem("accessTokenExpiresAt")
+    );
   }
 
-  if (!accessToken || (accessToken && Date.now() >= accessToken.expiresAt)) {
+  if (!accessToken || (accessToken && Date.now() >= accessTokenExpiresAt)) {
     const { data } = await axios.get("/token");
-    accessToken = {
-      accessToken: data.access_token,
-      expiresAt: Number(data.expires_in) * 1000 + Date.now(),
-    };
-    window.localStorage.setItem("accessToken", accessToken.accessToken);
-    window.localStorage.setItem("accessTokenExpiresAt", accessToken.expiresAt);
+    accessToken = data.access_token;
+    accessTokenExpiresAt = Number(data.expires_in) * 1000 + Date.now();
+    window.localStorage.setItem("accessToken", accessToken);
+    window.localStorage.setItem("accessTokenExpiresAt", accessTokenExpiresAt);
   }
 
-  return accessToken.accessToken;
+  return accessToken;
 };
 
 class ApiClient {
   constructor() {
     methods.forEach((method) => {
-      this[method] = (path, { headers = {}, data } = {}) =>
+      this[method] = (path, { headers = {}, params, data } = {}) =>
         new Promise((resolve, reject) => {
           getAccessToken()
             .then((accessToken) => accessToken)
             .then((accessToken) => {
               headers.Authorization = `Bearer ${accessToken}`;
-              axios[method](formatUrl(path), { headers })
+              axios[method](formatUrl(path), { headers, params, data })
                 .then(resolve)
                 .catch(reject);
             });
