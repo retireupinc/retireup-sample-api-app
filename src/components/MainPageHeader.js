014051bfcfca832da7f1b728ddb42cc6078b5569
Vicente Chavez <vchavez92780@gmail.com>
2021-03-21 13:27:17 -0400

    Cleaned up props passed down

--- src/components/MainPageHeader.js
+++ src/components/MainPageHeader.js
@@ -1,9 +1,9 @@
 function MainPageHeader(props) {
-  const { label, render } = props;
+  const { label, render, ...rest } = props;
   return (
     <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-5 border-bottom">
       <h1 className="h2">{label}</h1>
-      {render ? render(props) : null}
+      {render ? render(rest) : null}
     </div>
   );
 }
