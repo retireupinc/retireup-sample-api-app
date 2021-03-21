64f2a3f57264413a72d962b875ffcabe5267f2af
Vicente Chavez <vchavez92780@gmail.com>
2021-03-21 02:46:32 -0400

    Support setting toolbar in page header

--- src/components/MainPageHeader.js
+++ src/components/MainPageHeader.js
@@ -1,8 +1,9 @@
 function MainPageHeader(props) {
-  const { label } = props;
+  const { label, render } = props;
   return (
     <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
       <h1 className="h2">{label}</h1>
+      {render ? render(props) : null}
     </div>
   );
 }
