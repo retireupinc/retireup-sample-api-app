4e3e09e25bdbc7b5c5504c53998d3466351325a6
Vicente Chavez <vchavez92780@gmail.com>
2021-03-21 11:45:57 -0400

    Removed unneccesary component

--- src/routes/Dashboard/IncomeGraph/index.js
+++ src/routes/Dashboard/IncomeGraph/index.js
@@ -1,11 +1,15 @@
 import React, { useContext } from "react";
 import styled from "styled-components";
 import _ from "lodash";
+import { ticks, tickStep } from "d3-array";
+import { scaleLinear } from "d3-scale";
 import useResizeObserver from "../../../hooks/useResizeObserver";
 import { calcTransitionRows } from "../../../utils";
 import DashboardContext from "../DashboardContext";
-import ChartContainer from "./ChartContainer";
 import Legend from "./Legend";
+import Chart from "./Chart";
+import YAxis from "./YAxis";
+import { margin } from "./variables";
 
 const GContainer = styled.div`
   height: 400px;
@@ -16,6 +20,22 @@ const GContainer = styled.div`
 const GraphContainer = styled.div`
   flex-grow: 1;
   position: relative;
+`;
+
+const ChartContainer = styled.div`
+  display: flex;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+const ChartAreaContainer = styled.div`
+  overflow-x: auto;
+  overflow-y: hidden;
+  flex-grow: 1;
+  height: 101.5%;
 `;
 
 function IncomeGraph(props) {
@@ -128,11 +148,46 @@ function IncomeGraph(props) {
       ? allrows
       : calcTransitionRows(allrows);
 
+  const chartMin = 0;
+  let chartYScale = 0;
+  let chartYAxisTicks = [];
+  if (size) {
+    const height = size.height - margin.top - margin.bottom;
+
+    let chartMax = _.maxBy(rows, (d) => d.total).total;
+
+    chartYAxisTicks = ticks(chartMin, chartMax, 5);
+    const distance = tickStep(chartMin, chartMax, 5);
+
+    if (chartMax > _.last(chartYAxisTicks)) {
+      chartMax = _.last(chartYAxisTicks) + distance;
+      chartYAxisTicks.push(chartMax);
+    }
+
+    chartYScale = scaleLinear([chartMin, chartMax], [height, 0]);
+  }
+
   return (
     <GContainer>
       <GraphContainer ref={ref}>
         {size && (
-          <ChartContainer household={household} size={size} rows={rows} />
+          <ChartContainer className="d-flex position-absolute">
+            <YAxis
+              size={size}
+              yScale={chartYScale}
+              yAxisTicks={chartYAxisTicks.slice(1)}
+            />
+            <ChartAreaContainer>
+              <Chart
+                household={household}
+                size={size}
+                rows={rows}
+                min={chartMin}
+                yScale={chartYScale}
+                yAxisTicks={chartYAxisTicks}
+              />
+            </ChartAreaContainer>
+          </ChartContainer>
         )}
       </GraphContainer>
       <Legend rows={rows} />
