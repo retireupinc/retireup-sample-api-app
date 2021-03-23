b651938f717cd50898d3ee80961601b674aa181a
Vicente Chavez <vchavez92780@gmail.com>
2021-03-23 15:34:42 -0400

    Added error route

--- src/routes/Error.js
+++ src/routes/Error.js
@@ -1,15 +1,30 @@
 import { useContext } from "react";
 import { Alert } from "react-bootstrap";
+import styled from "styled-components";
 import { authContext } from "../contexts/AuthContext";
 import withGuestRoute from "../components/WithGuestRoute";
 import withPrivateRoute from "../components/WithPrivateRoute";
 
-function ErrorMessage() {
+const StyledMainContainer = styled.div`
+  width: 100%;
+  padding: 15px;
+  margin: auto;
+`;
+
+function ErrorMessage(props) {
+  const { auth } = useContext(authContext);
   return (
-    <Alert variant="danger">
-      <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-      <p>Looks like the page is not valid.</p>
-    </Alert>
+    <StyledMainContainer>
+      <Alert variant="danger">
+        <Alert.Heading>Oh snap!</Alert.Heading>
+        <p>You got an error! The page was not found.</p>
+        {!auth?.isAuthenticated ? (
+          <Alert.Link href="/login">Click here to login</Alert.Link>
+        ) : (
+          <Alert.Link href="/">Click here to return</Alert.Link>
+        )}
+      </Alert>
+    </StyledMainContainer>
   );
 }
 
