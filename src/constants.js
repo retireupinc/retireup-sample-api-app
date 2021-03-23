01b165c5799d6ed53f4a4742e2b52a0edda4aec0
Vicente Chavez <vchavez92780@gmail.com>
2021-03-23 14:36:25 -0400

    Added file to store constants

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const DEFAULT_USER_AUTH = {
+  name: "",
+  email: "",
+  accessToken: "",
+  accessTokenExpiresAt: -1,
+  isAuthenticated: false,
+};
+
+export const USER_AUTH_LOCAL_STORAGE_KEY = "UserAuth";
