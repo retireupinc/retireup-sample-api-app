564f580a499987253657659e29f31672ccfedbbe
Vicente Chavez <vchavez92780@gmail.com>
2021-03-23 14:40:36 -0400

    Added logout route

---
+++ src/routes/Logout.js
@@ -0,0 +1,23 @@
+import { useContext, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
+import { authContext } from "../contexts/AuthContext";
+import withMainLayout from "../components/WithMainLayout";
+
+function Logout() {
+  const { setUnauthStatus } = useContext(authContext);
+  useEffect(() => {
+    setTimeout(() => {
+      setUnauthStatus();
+    }, 1000);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+      <Spinner animation="border" variant="primary" role="status">
+        <span className="sr-only">Please wait...</span>
+      </Spinner>
+    </div>
+  );
+}
+
+export default withMainLayout(Logout);
